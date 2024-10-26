bda8ebb8f64fa4b66641664e8ccfccaa8062b970
Faisal8979 <faisal897913@gmail.com>
2024-10-26 19:35:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,4 +19,7 @@ app.post('/api/checkPaymentStatus', (req, res) => {
     }
 });
 
-
+app.get("/", (req, res) => {
+    res.send("<h1>Hello World</h1>")
+})
+app.listen(8000, () => console.log('Server running on port 8000'));
